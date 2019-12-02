ba768b4d5b079752accc384d19ca1c11684e6b1c
Łukasz Antończyk <lukasz.antonczyk@fingo.pl>
2019-12-02 10:07:53 +0100

    Day 1 part 2

--- lib/aoc2019/day1.ex
+++ lib/aoc2019/day1.ex
@@ -2,8 +2,21 @@ defmodule AoC2019.Day1 do
   def fuel_required do
     AoC2019.input(1)
     |> Stream.map(&String.to_integer/1)
-    |> Stream.map(&fuel_for/1)
+    |> Stream.map(&fuel_required/1)
     |> Enum.sum()
+  end
+
+  @doc """
+  iex> AoC2019.Day1.fuel_required(14)
+  2
+  iex> AoC2019.Day1.fuel_required(100756)
+  50346
+  """
+  def fuel_required(mass, total_fuel \\ 0) do
+    case fuel_for(mass) do
+      fuel when fuel < 0 -> total_fuel
+      fuel -> fuel_required(fuel, total_fuel + fuel)
+    end
   end
 
   @doc """
