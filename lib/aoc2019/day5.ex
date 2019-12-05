d6cc0c45039e4dfe2d7d878b23dd5b18b38191d4
Łukasz Antończyk <lukasz.antonczyk@fingo.pl>
2019-12-05 12:46:40 +0100

    Day 5 part 2

--- lib/aoc2019/day5.ex
+++ lib/aoc2019/day5.ex
@@ -1,9 +1,17 @@
 defmodule AoC2019.Day5 do
   @day 5
 
-  @input_value 1
-
-  def test_diagnostic(program \\ AoC2019.read(@day)) do
+  @doc """
+  iex> program = "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99"
+  iex> AoC2019.Day5.test_diagnostic(program, 7)
+  999
+  iex> AoC2019.Day5.test_diagnostic(program, 8)
+  1000
+  iex> AoC2019.Day5.test_diagnostic(program, 9)
+  1001
+  """
+  def test_diagnostic(program \\ AoC2019.read(@day), input) do
+    Process.put(:input, input)
     program
     |> String.split(",", trim: true)
     |> Enum.map(&String.to_integer/1)
@@ -14,20 +22,34 @@ defmodule AoC2019.Day5 do
   @multiply 2
   @input 3
   @output 4
+  @jump_if_true 5
+  @jump_if_false 6
+  @less_than 7
+  @equals 8
   @finish 99
 
   @position_mode 0
   @immediate_mode 1
 
   def perform(memory, instruction_pointer \\ 0) do
+    case perform_instruction(memory, instruction_pointer) do
+      {:done, _memory, _instruction_pointer} ->
+        Process.get(:output)
+
+      {:ok, memory, instruction_pointer} ->
+        perform(memory, instruction_pointer)
+    end
+  end
+
+  def perform_instruction(memory, instruction_pointer) do
     instruction = read_next_instruction(Enum.drop(memory, instruction_pointer))
 
-    case perform_instruction(memory, instruction) do
-      {:done, memory} ->
-        memory
+    case do_perform_instruction(memory, instruction) do
+      {status, memory} ->
+        {status, memory, instruction_pointer + instruction_pointer_offset(instruction)}
 
-      {:ok, memory} ->
-        perform(memory, instruction_pointer + instruction_pointer_offset(instruction))
+      {_status, _memory_, _instruction_pointer} = result ->
+        result
     end
   end
 
@@ -44,8 +66,6 @@ defmodule AoC2019.Day5 do
   {2, [0, 1, 0]}
   iex> AoC2019.Day5.parse_instruction(1102)
   {2, [1, 1, 0]}
-  iex> AoC2019.Day5.parse_instruction(3)
-  {3, [0, 0, 0]}
   iex> AoC2019.Day5.parse_instruction(99)
   {99, [0, 0, 0]}
   """
@@ -66,38 +86,66 @@ defmodule AoC2019.Day5 do
   defp instruction_params_count(@multiply), do: 3
   defp instruction_params_count(@input), do: 1
   defp instruction_params_count(@output), do: 1
+  defp instruction_params_count(@jump_if_true), do: 2
+  defp instruction_params_count(@jump_if_false), do: 2
+  defp instruction_params_count(@less_than), do: 3
+  defp instruction_params_count(@equals), do: 3
   defp instruction_params_count(@finish), do: 0
 
   defp instruction_pointer_offset({opcode, _}) do
     instruction_params_count(opcode) + 1
   end
 
-  def perform_instruction(memory, {@add, params}) do
-    param1 = get_memory(memory, Enum.at(params, 0))
-    param2 = get_memory(memory, Enum.at(params, 1))
-
-    {:ok, set_memory(memory, Enum.at(params, 2), param1 + param2)}
+  defp do_perform_instruction(memory, {@add, [param1, param2, param3]}) do
+    result = get_memory(memory, param1) + get_memory(memory, param2)
+    {:ok, set_memory(memory, param3, result)}
   end
 
-  def perform_instruction(memory, {@multiply, params}) do
-    param1 = get_memory(memory, Enum.at(params, 0))
-    param2 = get_memory(memory, Enum.at(params, 1))
-
-    {:ok, set_memory(memory, Enum.at(params, 2), param1 * param2)}
+  defp do_perform_instruction(memory, {@multiply, [param1, param2, param3]}) do
+    result = get_memory(memory, param1) * get_memory(memory, param2)
+    {:ok, set_memory(memory, param3, result)}
   end
 
-  def perform_instruction(memory, {@input, params}) do
-    {:ok, set_memory(memory, Enum.at(params, 0), @input_value)}
+  defp do_perform_instruction(memory, {@input, [param]}) do
+    {:ok, set_memory(memory, param, Process.get(:input))}
   end
 
-  def perform_instruction(memory, {@output, params}) do
-    IO.inspect({:output, get_memory(memory, Enum.at(params, 0))})
+  defp do_perform_instruction(memory, {@output, [param]}) do
+    IO.inspect({:output, get_memory(memory, param)})
+    Process.put(:output, get_memory(memory, param))
 
-    Inspect.Opts
     {:ok, memory}
   end
 
-  def perform_instruction(memory, {@finish, _}) do
+  defp do_perform_instruction(memory, {@jump_if_true, [param1, param2]}) do
+    case get_memory(memory, param1) do
+      0 -> {:ok, memory}
+      _ -> {:ok, memory, get_memory(memory, param2)}
+    end
+  end
+
+  defp do_perform_instruction(memory, {@jump_if_false, [param1, param2]}) do
+    case get_memory(memory, param1) do
+      0 -> {:ok, memory, get_memory(memory, param2)}
+      _ -> {:ok, memory}
+    end
+  end
+
+  defp do_perform_instruction(memory, {@less_than, [param1, param2, param3]}) do
+    case get_memory(memory, param1) < get_memory(memory, param2) do
+      true -> {:ok, set_memory(memory, param3, 1)}
+      _ -> {:ok, set_memory(memory, param3, 0)}
+    end
+  end
+
+  defp do_perform_instruction(memory, {@equals, [param1, param2, param3]}) do
+    case get_memory(memory, param1) == get_memory(memory, param2) do
+      true -> {:ok, set_memory(memory, param3, 1)}
+      _ -> {:ok, set_memory(memory, param3, 0)}
+    end
+  end
+
+  defp do_perform_instruction(memory, {@finish, _}) do
     {:done, memory}
   end
 
