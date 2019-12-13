a5892331f6bff9870045874dad3b95585e43feeb
Łukasz Antończyk <lukasz.antonczyk@fingo.pl>
2019-12-13 12:00:03 +0100

    Day 13 clean up

--- lib/aoc2019/day13.ex
+++ lib/aoc2019/day13.ex
@@ -15,44 +15,50 @@ defmodule AoC2019.Day13 do
     input = String.replace_prefix(input, "1", "2")
     {:ok, pid} = IntcodeComputer.start_program(input, ask_for_input: true)
 
-    acc = %{
-      map: %{},
+    %{
+      computer: pid,
+      screen: %{},
+      score: 0,
       ball: {0, 0},
       paddle: {0, 0}
     }
-
-    Enum.reduce_while(Stream.iterate(pid, & &1), acc, fn pid, acc ->
-      case IntcodeComputer.get_output(pid) do
-        :done ->
-          {:halt, acc}
-
-        :waiting_for_input ->
-          move_paddle(pid, acc)
-          {:cont, acc}
-
-        x ->
-          y = IntcodeComputer.get_output(pid)
-          tile = IntcodeComputer.get_output(pid)
-          {:cont, update(acc, x, y, tile)}
-      end
-    end)
-    |> get_in([:map, :score])
+    |> do_play()
+    |> Map.get(:score)
   end
 
-  defp update(%{map: map} = acc, x, y, @ball) do
-    %{acc | map: update(map, x, y, @ball), ball: {x, y}}
+  defp do_play(%{computer: pid} = state) do
+    case IntcodeComputer.get_output(pid) do
+      :done ->
+        state
+
+      :waiting_for_input ->
+        move_paddle(state)
+        do_play(state)
+
+      x ->
+        y = IntcodeComputer.get_output(pid)
+        tile = IntcodeComputer.get_output(pid)
+        do_play(update(state, x, y, tile))
+    end
   end
 
-  defp update(%{map: map} = acc, x, y, @paddle) do
-    %{acc | map: update(map, x, y, @paddle), paddle: {x, y}}
+  defp update(state, -1, 0, score) do
+    %{state | score: score}
   end
 
-  defp update(%{map: map} = acc, x, y, tile) do
-    %{acc | map: update(map, x, y, tile)}
+  defp update(%{screen: screen} = state, x, y, @ball) do
+    %{state | screen: update_screen(screen, x, y, @ball), ball: {x, y}}
   end
 
-  defp update(map, -1, 0, score), do: Map.put(map, :score, score)
-  defp update(map, x, y, tile), do: Map.put(map, {x, y}, tile(tile))
+  defp update(%{screen: screen} = state, x, y, @paddle) do
+    %{state | screen: update_screen(screen, x, y, @paddle), paddle: {x, y}}
+  end
+
+  defp update(%{screen: screen} = state, x, y, tile) do
+    %{state | screen: update_screen(screen, x, y, tile)}
+  end
+
+  defp update_screen(screen, x, y, tile), do: Map.put(screen, {x, y}, tile(tile))
 
   defp tile(@empty), do: " "
   defp tile(@wall), do: "|"
@@ -60,7 +66,7 @@ defmodule AoC2019.Day13 do
   defp tile(@paddle), do: "_"
   defp tile(@ball), do: "o"
 
-  defp move_paddle(pid, %{ball: {bx, _}, paddle: {px, _}}) do
+  defp move_paddle(%{computer: pid, ball: {bx, _}, paddle: {px, _}}) do
     cond do
       bx < px -> IntcodeComputer.send_input(pid, -1)
       bx > px -> IntcodeComputer.send_input(pid, 1)
